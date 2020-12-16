299fd691a9c5b33d1dc0a670fefc83ddf3cb842a
bsski <50927653+BSski@users.noreply.github.com>
2020-12-16 22:53:44 +0100

    16.12

--- gym-tdh/gym_tdh/envs/tdh_env.py
+++ gym-tdh/gym_tdh/envs/tdh_env.py
@@ -14,15 +14,16 @@ class TdhEnv(gym.Env):
                                 [-6, 24], # DECEIT
                                 [-2, -2]  # MUTUAL DECEIT
                                 ]
-        self.action_space = spaces.Box(low=np.array([0]), high=np.array([1]), dtype=np.int)
+        self.action_space = spaces.Box(low=np.array([0]), high=np.array([1]), dtype=np.int)  # DQN
+        # self.action_space = spaces.Box(low=np.array([-1]), high=np.array([1]), dtype=np.float64)  # DDPG
         # observation space, czyli zakres wartości, które przekazuję sieciom agentów jako input w pliku simultaneous.py (funkcja forward)
-        self.observation_space = spaces.Box(low=np.array([0, 0, 0, -6]), high=np.array([1, 20, 4, 34]), dtype=np.int)
+        # [actions, step, enemy_hive, enemy_rewards, my_index, enemy_index]
+        self.observation_space = spaces.Box(low=np.array([0, 0, 0, -6, 0, 0]), high=np.array([1, 20, 9, 24, np.inf, np.inf]), dtype=np.int)
         self.steps_counter = -1
         self.episodes_counter = 1
         self.game = []
         self.rewards = [0, 0]
         self.observation = [random.randint(0, 1), random.randint(0, 1), self.steps_counter, [0, 0]]
-        # self.cash = 200
         self.rewards_list = []
         self.seed()
         self.reset()
@@ -34,8 +35,13 @@ class TdhEnv(gym.Env):
     def step(self, actions):
         if self.episodes_counter == 1 and self.steps_counter == 0:
             print("Rewards setting:", self.rewards_setting)
+
         action_0 = actions[0]
         action_1 = actions[1]
+        print("act0 before change:", action_0, "act1 before change:", action_1)
+        action_0 = 1 if action_0 > 0 else 0
+        action_1 = 1 if action_1 > 0 else 0
+        print("act0 after change:", action_0, "act1 after change:", action_1)
 
         if action_0 == 0 and action_1 == 0:
             rewards = self.rewards_setting[0]
@@ -46,9 +52,9 @@ class TdhEnv(gym.Env):
         if action_0 == 1 and action_1 == 1:
             rewards = self.rewards_setting[3]
 
-        self.game.append(actions)
+        self.game.append([action_0, action_1])
         self.steps_counter += 1
-        self.observation = [actions[1], actions[0], self.steps_counter, rewards]
+        self.observation = [action_1, action_0, self.steps_counter, rewards]
 
         if self.steps_counter == 19:
             player_zero = ""
