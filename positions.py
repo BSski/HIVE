b1ebb8d991efac74113b490cf1104a83055c8cdf
bsski <50927653+BSski@users.noreply.github.com>
2021-08-13 01:18:00 +0200

    few PEP8 changes

--- positions.py
+++ positions.py
@@ -18,7 +18,7 @@ for j in range((int(diameter/2)+1),(diameter*2)-1):
         
     carnivores_pos_hex.append(row)
 
-
+    
 # Complete boundary tiles list.
 for i in range(len(carnivores_pos_hex[0])):
     boundary_tiles.append([i, 0])
